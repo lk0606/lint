b171916a8db19480dc65b533aa18a5e9c994b5e6
bantang <bantang@zhuangdianwenhua.com>
2021-05-27 16:05:51 +0800

    chore: 去掉无用rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production'
-console.log('isProd :>> ', isProd)
 module.exports = {
     root: true,
     extends: ['eslint-config-ali', 'prettier'],
-    rules: {
-        'no-console': isProd ? 2 : 1,
-        'no-debugger': isProd ? 2 : 1,
-    },
+    rules: {},
 }
